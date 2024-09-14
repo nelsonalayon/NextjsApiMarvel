598b7c30644ae7e1ea2573cb5c2fa5e7f85a3923
nelsonalayon <nelson.alayon@gmail.com>
2024-09-14 11:37:12 -0500

    type add to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,11 @@
+import React, { ReactNode } from 'react';
 
-export default function Button ( { children } ) {
+type ButtonProps = {
+  children: ReactNode;
+};
+
+
+export default function Button ( { children }: ButtonProps ) {
   return (
     <button className="w-max bg-red-600 hover:bg-red-700 text-white py-3 px-6 text-lg font-semibold m-1">
       {children}
